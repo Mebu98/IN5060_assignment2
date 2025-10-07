393d7f4fa24aa8667cd0ab7c5679269c07df4809
Johannes <me.jo.bu@gmail.com>
2025-10-07 14:32:18 +0200

    basic fig

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import glob
 import pandas as pd
 import csv
+import plotly.express as px
 
-csv_files_path = "./4G_2023_passive/*.csv"
-all_csv_files = glob.glob(csv_files_path)
 
-for file_path in all_csv_files:
-    print(f"Processing file: {file_path}")
+with open('4G_2023_passive/location_4_od_capacity_iw_tti_0.csv', newline='') as csvfile:
+    csv = pd.read_csv(csvfile)
 
-# with open('./4G_2023_passive/location_1_is_capacity_dl+ul_5gnr_tti_1.csv', newline='') as csvfile:
-#     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-#     for row in spamreader:
-#        print(', '.join(row))
+
+fig = px.scatter_map(csv, lat="Latitude", lon="Longitude", hover_name="RSRP", color='RSRP', zoom=15)
+fig.update_layout(map_style="open-street-map")
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+fig.show()
+
+# csv_files_path = "./4G_2023_passive/*.csv"
+# all_csv_files = glob.glob(csv_files_path)
+#
+# for file_path in all_csv_files:
+#     print(f"Processing file: {file_path}")
+#
+
