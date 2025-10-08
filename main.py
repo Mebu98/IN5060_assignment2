2479456749e973df78020b68852842a6c49df556
Johannes <me.jo.bu@gmail.com>
2025-10-08 13:54:52 +0200

    multiple metrics for violin plot

--- main.py
+++ main.py
@@ -2,31 +2,41 @@ import glob
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
-from pygments.lexers import go
+from matplotlib.pyplot import legend
+import plotly.graph_objects as go
 
 from color_maps import operator_color_map
 
 pio.renderers.default = "browser"
 
-for i in range(1,2):
+metrics = ["RSRQ", "RSRP", "SINR"]
+
+for i in range(4,5):
     try:
-        csv_files_path = f"./4G_2023_passive/location_{i}_*.csv"
+        csv_files_path = f"./4G_2023_passive/location_{i}_od_capacity_*.csv"
         all_csv_files = glob.glob(csv_files_path)
 
         csv = pd.concat([pd.read_csv(file) for file in all_csv_files], ignore_index=True)
-        print(csv)
         csv.sort_values(by=['MNC'], inplace=True)
 
-        fig = px.scatter_map(csv, title=i, lat="Latitude", lon="Longitude", hover_name="RSRP", color='RSRP', zoom=15)
-        fig.update_layout(map_style="open-street-map")
-        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-        fig.show()
+        # operators = csv['MNC'].unique()
+        # operator_csvs = [csv[csv['MNC'] == op] for op in operators]
+        #
+        # for op_csv in operator_csvs:
+        #     print(op_csv.size)
+        #
+        #     fig = px.scatter_map(op_csv, title=f'location {i} {op_csv['MNC'].unique()[0]}', labels='MNC', lat="Latitude", lon="Longitude", hover_name=metric, color=metric, zoom=15)
+        #     fig.update_layout(map_style="open-street-map")
+        #     fig.update_layout(margin={"r": 50, "t": 50, "l": 50, "b": 50})
+        #     fig.show()
 
-        operators = csv['MNC'].unique()
-        operator_csvs = [csv[csv['MNC'] == op] for op in operators]
+        for metric in metrics:
+            vp = px.violin(csv, title=i, box=True, x=csv['MNC'], y=metric, color="MNC",
+                           color_discrete_map=operator_color_map)
+            vp.show()
 
-        vp = px.violin(csv, title=i, box=True, x=csv['MNC'], y='RSRP', color="MNC", color_discrete_map=operator_color_map)
-        vp.show()
-    except Exception:
+
+    except Exception as e:
         print(f"{i} failed")
+        print(e)
         continue
