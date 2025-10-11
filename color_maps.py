281b3aaa7fcc364473b9f620f9b3dd4f551cd82a
Johannes <me.jo.bu@gmail.com>
2025-10-11 20:41:59 +0200

    Uhhhh, yeah. Should open all graphs in streamlit (a single tab instead of 50) now. Also done some other things

--- color_maps.py
+++ color_maps.py
@@ -1,15 +1,22 @@
+
+
 operator_color_map = {
     'OP1 (Tim)': "red",
     'OP2 (Vodafone)': "blue",
     'OP3 (Illiad)': "orange",
-    'OP4 (Wind)': "green"
+    'OP4 (Wind)': "green",
+
+    '"Op"[1]': "red",
+    '"Op"[2]': "blue",
+    '"Op"[3]': "orange",
+    '"Op"[4]': "green"
 }
 
 metric_range_map = {
     "RSRQ": [-50, 0],
     "RSRP": [-160, -60],
-    "SINR": [-50, 30],
-    "SSS-RSRQ": [-50, 0],
-    "SSS_RSRP" : [-160, -60],
-    "SSS-SINR" : [-50, 30]
+    "SINR": [-50, 35],
+    "SSS-RSRQ": [-100, 0],
+    "SSS_RSRP" : [-190, -65],
+    "SSS-SINR" : [-80, 35]
 }
--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import glob
-import json
-from unittest import case
+import os.path
+import sys
+import uuid
 
+import numpy as np
 import pandas as pd
 import plotly.express as px
-import plotly.io as pio
-#from matplotlib.pyplot import legend
 import plotly.graph_objects as go
-from matplotlib.pyplot import figure
+import plotly.io as pio
 import scikit_posthocs as sp
 import scipy as scipy
-from statsmodels.compat import asstr
-import seaborn as sns
-import matplotlib.pyplot as plt
+import streamlit as st
+from streamlit.web.cli import main
 
-from color_maps import operator_color_map
 from color_maps import metric_range_map
+from color_maps import operator_color_map
+
+if __name__ == '__main__':
+    if not st.runtime.exists():
+        sys.argv = ["streamlit", "run", sys.argv[0]]
+        sys.exit(st.web.cli.main())
+
 
 pio.renderers.default = "browser"
-
 
 operators = {
     '"Op"[1]': 'OP1 (Tim)',
@@ -26,7 +30,6 @@ operators = {
     '"Op"[3]': 'OP3 (Illiad)',
     '"Op"[4]': 'OP4 (Wind)',
 }
-#"RSRQ", "RSRP",
 metrics_4G = ['RSRQ','RSRP',"SINR"]
 metrics_5G = ["SSS-RSRQ", "SSS_RSRP", "SSS-SINR"]
 metric_map = {
@@ -34,6 +37,8 @@ metric_map = {
         "RSRP": ("RSRP", "SSS_RSRP"),
         "SINR": ("SINR", "SSS-SINR")
     }
+
+
 
 def plot_graphs(signal, csv):
     metrics = None
@@ -63,8 +68,14 @@ def plot_graphs(signal, csv):
             minimum = csv.groupby('MNC')[metric].min()
             vp.add_annotation(x=x + .33, y=median[op], showarrow=False, text=f'median: {median[op]:.2f}',
                               font=dict(size=14))
-        vp.show()
+        # vp.show()
 
+        st.plotly_chart(vp)
+
+        dunns = statistical_difference(signal, csv, metric)
+        dunns_heatmap = px.imshow(dunns['result'], text_auto=True, title=str(dunns['kruskal']))
+
+        st.plotly_chart(dunns_heatmap)
 
 def one_OP_Compare_Location():
     loaction_numb=[]
@@ -113,28 +124,30 @@ def one_OP_Compare_Location():
                        range_y=metric_range_map[metric],)
         vp.show()
 
-def OP_Compare_Location_4_5(op):
-    loaction_numb=[]
+def OP_Compare_Location_4g_5g(op):
     all_data_4g=[]
     all_data_5g = []
-    df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/violin_data.csv")
-    fig = go.Figure()
 
     for i in range(1, 16):
         try:
-            csv_files_path = f"./4G_2023_passive/location_{i}_od_*.csv"
+            csv_files_path = f"./4G_2023_passive/location_{i}_od_ookla_*.csv"
             all_csv_files = glob.glob(csv_files_path)
+
+            # Skip for loop iteration if no csv files for this index.
+            if all_csv_files is None or len(all_csv_files) == 0:
+                continue
+
             csv4G = pd.concat([pd.read_csv(file) for file in all_csv_files], ignore_index=True)
-            csv4G['Location'] = str(i)
+            csv4G['Location'] = f'location_{i}'
             csv4G['type'] = '4G'
             filtered_csv4G = (csv4G[csv4G['MNC'] == op])
             all_data_4g.append(filtered_csv4G)
 
-            csv_files_path = f"./5G_2023_passive/location_{i}_od_*.csv"
+            csv_files_path = f"./5G_2023_passive/location_{i}_od_ookla_*.csv"
             all_csv_files = glob.glob(csv_files_path)
 
             csv5G = pd.concat([pd.read_csv(file) for file in all_csv_files], ignore_index=True)
-            csv5G['Location'] = i
+            csv5G['Location'] = f'location_{i}'
             # csv5G.rename(columns={"SSS_RSRP": "RSRP"}, inplace=True)
             # csv5G.rename(columns={"SSS-RSRQ": "RSRQ"}, inplace=True)
             # csv5G.rename(columns={"SSS-SINR": "SINR"}, inplace=True)
@@ -152,14 +165,14 @@ def OP_Compare_Location_4_5(op):
 
     full_data_4g = pd.concat(all_data_4g, ignore_index=True)
     full_data_5g = pd.concat(all_data_5g, ignore_index=True)
-    for metric in metrics_4G:
 
+    for metric in metrics_4G:
         fig = go.Figure()
         fig.add_trace(go.Violin(x=full_data_4g['Location'],
                                 y=full_data_4g[metric],
                                 legendgroup='4G', scalegroup='4G', name='4G',
                                 side='negative',
-                                line_color='orange')
+                                line_color=operator_color_map[op])
                       )
         fig.add_trace(go.Violin(x=full_data_5g['Location'],
                                 y=full_data_5g[metric],
@@ -177,20 +190,13 @@ def OP_Compare_Location_4_5(op):
             font=dict(size=14),
             legend_title="Technology",
             yaxis_range=metric_range_map[metric],
-            xaxis_range=[-0.5, 3.5],
+            xaxis_range=[-0.5, len(full_data_4g['Location'].unique()) -.5],
         )
 
-        fig.show()
-        # vp = px.violin(full_data,
-        #                 title=f"Operator MNC: Illian - Metric: {metric}",
-        #                box=True,
-        #                x='Location',
-        #                y=metric)
-        # vp.show()
+        # fig.show()
+        st.plotly_chart(figure_or_data=fig, use_container_width=True)
 
 
-#one_OP_Compare_Location()
-OP_Compare_Location_4_5('"Op"[3]')
 # for i in range(4,5):
 #     try:
 #         csv_files_path = f"./4G_2023_passive/location_{i}_od_capacity_*.csv"
@@ -232,40 +238,44 @@ OP_Compare_Location_4_5('"Op"[3]')
 
 #one_OP_Compare_Location()
 
-def statistical_difference(signal, csv):
+def statistical_difference(signal, csv, metric):
 
-    metrics = metrics_4G
+    ops = []
 
-    match signal:
-        case "4G": metrics = metrics_4G
-        case "5G": metrics = metrics_5G
+    for op in operators.values():
+        ops.append(csv[csv['MNC'] == op][metric].tolist())
 
-    for metric in metrics:
+    kruskal = scipy.stats.mstats.kruskal(*ops)
+    kruskal = {'statistic': str(kruskal[0]), 'p-value': str(kruskal[1])}
+    print(f'location:{i} signal: {signal}, metric:{metric},{kruskal}')
 
-        ops = []
+    # dunns = scipy.stats.dunnett(*ops)
+    dunns = sp.posthoc_dunn(csv, group_col='MNC', val_col=metric, p_adjust='simes-hochberg')
 
-        for op in operators.values():
-            ops.append(csv[csv['MNC'] == op][metric].tolist())
+    # Round since plotly sometimes reads small numbers as letters for some reason...
+    dunns = dunns.round(decimals=5)
 
-        kruskal = scipy.stats.kruskal(*ops)
-        print(f'location:{i} signal: {signal}, metric:{metric},{kruskal}')
+    dunns_result = {
+        'location': i,
+        'signal': signal,
+        'metric': metric,
+        'kruskal': kruskal,
+        'result': dunns,
+    }
 
-        dunns = sp.posthoc_dunn(csv, group_col='MNC', val_col=metric, p_adjust='holm')
-        print(dunns)
-        # sns.heatmap(dunns, annot=True)
-        fig = px.imshow(dunns, title=f'location:{i} signal: {signal}, metric:{metric}, {kruskal}', text_auto=True)
-        fig.show()
-
+    return dunns_result
 
 for i in range(1,15):
     try:
-        csv_files_path = f"./4G_2023_passive/location_{i}_od_capacity_*.csv"
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        csv_files_path = f"{base_path}\\4G_2023_passive\\location_{i}_od_ookla_*.csv"
         all_csv_files = glob.glob(csv_files_path)
 
         csv4G = pd.concat([pd.read_csv(file) for file in all_csv_files], ignore_index=True)
         csv4G.sort_values(by=['MNC'], inplace=True)
         csv4G.replace({'MNC': operators}, inplace=True)
-        print(csv4G['MNC'].unique())
+        plot_graphs(signal="4G", csv=csv4G)
+
 
         csv_files_path = f"./5G_2023_passive/location_{i}_od_capacity_*.csv"
         all_csv_files = glob.glob(csv_files_path)
@@ -273,15 +283,13 @@ for i in range(1,15):
         csv5G = pd.concat([pd.read_csv(file) for file in all_csv_files], ignore_index=True)
         csv5G.sort_values(by=['MNC'], inplace=True)
         csv5G.replace({'MNC': operators}, inplace=True)
+        plot_graphs(signal="5G", csv=csv5G)
 
-        # plot_graphs(signal="4G", csv=csv4G)
-        # plot_graphs(signal="5G", csv=csv5G)
-
-        # statistical_difference(signal="5G", csv=csv5G)
 
     except Exception as e:
         print(f"{i} failed")
         print(e)
         continue
 
-plt.show()
+OP_Compare_Location_4g_5g('"Op"[3]')
+
